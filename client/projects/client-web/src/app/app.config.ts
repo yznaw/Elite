8b0c8535bfdc42714e66ed083c3b52dffd316a4e
yznaw <yazan_awadeen@yahoo.com>
2026-06-06 16:45:15 +0300

    Update app.config.ts

--- client/projects/client-web/src/app/app.config.ts
+++ client/projects/client-web/src/app/app.config.ts
@@ -1,12 +1,18 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        anchorScrolling: 'enabled',
+        scrollPositionRestoration: 'top',
+      }),
+    ),
     provideHttpClient(withFetch()),
     provideAnimations(),
   ],
